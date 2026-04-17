99a40d446f65327e7464ff8eb25839f93449b2d7
vtu26071 <vtu26071@veltech.edu.in>
2026-04-17 22:55:15 +0530

    Implement method to add last digits of two integers

---
+++ UserMainCode.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode{
+public int addLastDigits(int input1, int input2) {
+    int lastDigit1 = Math.abs(input1) % 10;
+    int lastDigit2 = Math.abs(input2) % 10;
+    return lastDigit1 + lastDigit2;
+}
+}
